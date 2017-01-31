ae14fd216f1cb6fc6202d72ff2cd5d67a7f1ea9c
Kay Li <s1408733@moyes.inf.ed.ac.uk>
2017-01-31 16:18:21 +0000

    PUT FIRMWARE.INO INSIDE THE FOLDER

---
+++ firmware/libraries/firmware/firmware.ino
@@ -0,0 +1,130 @@
+#include "SerialCommand.h"
+#include "SDPArduino.h"
+#include <Wire.h>
+#include <Arduino.h>
+#include <I2CPort.h>
+
+
+//Kickers in back
+//define FRONT 1
+//define RIGHT 7
+//define BACK 5
+//define LEFT 3
+
+//Kickers in front
+#define FRONT 0
+#define RIGHT 1
+#define BACK 2
+#define LEFT 3
+
+#define KICKERS 4
+#define SPEAKER 5
+
+#define KICKERDELAY 10
+
+boolean requestStopKick = 0;
+boolean kickerStatus = 0;
+
+int zeroPosition;
+
+#define ROTARY_SLAVE_ADDRESS 5
+#define ROTARY_COUNT 6
+#define PRINT_DELAY 200
+
+// Initial motor position is 0.
+int positions[ROTARY_COUNT] = {0};
+
+int run = 0;
+
+SerialCommand sCmd;
+
+void loop(){
+  sCmd.readSerial();
+}
+
+
+void dontMove(){
+  motorControl(FRONT, 0);
+  motorControl(BACK, 0);
+  motorControl(LEFT, 0);
+  motorControl(RIGHT, 0);
+}
+
+void spinmotor(){
+  int motor = atoi(sCmd.next());
+  int power = atoi(sCmd.next());
+  motorForward(motor, power);
+}
+
+void motorControl(int motor, int power){
+  if (power == 0){
+    motorStop(motor);
+  } else if (power > 0){
+    motorForward(motor, power);
+  } else {
+    motorBackward(motor, -power);
+  }
+}
+
+void rationalMotors(){
+  int front = atoi(sCmd.next());
+  int back  = atoi(sCmd.next());
+  int left  = atoi(sCmd.next());
+  int right = atoi(sCmd.next());
+  motorControl(FRONT, -front);
+  motorControl(BACK, -back);
+  motorControl(LEFT, left);
+  motorControl(RIGHT, -right);
+}
+
+void pingMethod(){
+  Serial.println("pang");
+}
+
+void kicker(){
+  int type = atoi(sCmd.next());
+  if(type == 0){
+    motorStop(KICKERS);
+  } else if (type == 1){
+    Serial.print("Starting From: ");
+    Serial.println(positions[0] % 40);
+    motorForward(KICKERS, 100);
+    kickerStatus = 1;
+  } else {
+    motorBackward(KICKERS, 100);
+    kickerStatus = -1;
+  }
+}
+
+void completeHalt(){
+  motorAllStop();
+  motorControl(FRONT, 0);
+  motorControl(BACK, 0);
+  motorControl(LEFT, 0);
+  motorControl(RIGHT, 0);
+}
+
+void setup(){
+  Wire.begin();
+  sCmd.addCommand("f", dontMove); 
+  sCmd.addCommand("h", completeHalt); 
+  sCmd.addCommand("motor", spinmotor); 
+  sCmd.addCommand("r", rationalMotors);
+  sCmd.addCommand("ping", pingMethod); 
+  sCmd.addCommand("kick", kicker); 
+
+  SDPsetup();
+  helloWorld();
+}
+
+void printMotorPositions() {
+  Serial.print("Motor positions: ");
+  for (int i = 0; i < ROTARY_COUNT; i++) {
+    Serial.print(positions[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+  delay(PRINT_DELAY);  // Delay to avoid flooding serial out
+}
+
+
