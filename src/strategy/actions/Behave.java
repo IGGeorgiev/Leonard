5f97c46cbe6b7f25f8827914104b92970dca61a3
Isabella Chan <s1330027@sms.ed.ac.uk>
2017-03-25 20:27:54 +0000

    added SAFE

--- src/strategy/actions/Behave.java
+++ src/strategy/actions/Behave.java
@@ -1,10 +1,13 @@
 package strategy.actions;
 
+import com.sun.tools.internal.jxc.ap.Const;
 import strategy.Strategy;
 import strategy.actions.offense.GoalKick;
 import strategy.actions.other.DefendGoal;
 import strategy.actions.other.GoToSafeLocation;
 import strategy.actions.offense.ShuntKick;
+import strategy.actions.other.GotoBall;
+import strategy.points.basicPoints.BallPoint;
 import strategy.robots.Fred;
 import strategy.robots.RobotBase;
 import vision.Ball;
@@ -17,7 +20,7 @@ import vision.tools.VectorGeometry;
  * Created by Simon Rovder
  */
 enum BehaviourEnum {
-    DEFEND, SHUNT, GOAL, SAFE, EMPTY, GETBALL
+    DEFEND, SHUNT, GOAL, SAFE, EMPTY
 }
 
 /**
@@ -53,7 +56,7 @@ public class Behave extends StatefulActionBase<BehaviourEnum> {
                 this.enterAction(new DefendGoal(this.robot), 0, 0);
                 break;
             case GOAL:
-                this.enterAction(new GoalKick(this.robot), 0, 0);
+                this.enterAction(new GotoBall(this.robot, new BallPoint()), 0, 0);
                 break;
             case SHUNT:
                 this.enterAction(new ShuntKick(this.robot), 0, 0);
@@ -62,6 +65,14 @@ public class Behave extends StatefulActionBase<BehaviourEnum> {
                 this.enterAction(new GoToSafeLocation(this.robot), 0, 0);
                 break;
         }
+    }
+
+    /** returns true if foe (possibly) has the ball and is on our side of the pitch */
+    public boolean foeATTACK(Robot foe, VectorGeometry ball, VectorGeometry ourGoal) {
+        if (foe.location.distance(ball) <= 10 && foe.location.distance(ourGoal) <= Constants.PITCH_WIDTH/2) {
+            return true;
+        }
+        return false;
     }
 
     @Override
@@ -73,31 +84,54 @@ public class Behave extends StatefulActionBase<BehaviourEnum> {
             Robot us = Strategy.world.getRobot(this.robot.robotType);
             if (us == null) {
                 // TODO: Angry yelling
-            } else {
-                VectorGeometry ourGoal = new VectorGeometry(-Constants.PITCH_WIDTH / 2, 0);
-                if (us.location.distance(ourGoal) > ball.location.distance(ourGoal)) {
-                    this.nextState = BehaviourEnum.SAFE;
-                } else {
-//                    if(Math.abs(ball.location.x) > Constants.PITCH_WIDTH/2 - 20 && Math.abs(ball.location.y) > Constants.PITCH_HEIGHT/2 - 20){
-//                        this.nextState = BehaviourEnum.SHUNT;
-//                    } else {
-                    boolean canKick = true;
-                    boolean closer = true;
-                    for (Robot r : Strategy.world.getRobots()) {
-                        if (r != null && r.type != RobotType.FRIEND_2 && r.velocity.length() < 1)
-                            canKick = canKick && r.location.distance(ball.location) > 5;
-                            closer = closer && us.location.distance(ball.location)< r.location.distance(ball.location);
-                    }
-                    if (canKick && (this.lastState != BehaviourEnum.DEFEND ||
-                            VectorGeometry.angle(ball.velocity, VectorGeometry.fromTo(ball.location, ourGoal)) > 2)
-                            || closer) {
-                        this.nextState = BehaviourEnum.GOAL;
-                    } else {
-                        this.nextState = BehaviourEnum.DEFEND;
-                    }
-//                    }
-                }
+                // tell friend that we are lost
+                return;
             }
+            VectorGeometry ballVG = new VectorGeometry(ball.location.x, ball.location.y);
+            Robot foe1 = Strategy.world.getRobot(RobotType.FOE_1);
+            Robot foe2 = Strategy.world.getRobot(RobotType.FOE_2);
+
+            VectorGeometry ourGoal = new VectorGeometry(-Constants.PITCH_WIDTH / 2, 0);
+
+            if (foeATTACK(foe1, ballVG, ourGoal) || foeATTACK(foe2, ballVG, ourGoal)) { // if enemy is close to ball, assuming they have the ball
+                // TODO: with a friend - decide who to DEFEND or ATTACK and send packets
+                System.out.println("Enemy has the ball : calling SAFE");
+                this.nextState = BehaviourEnum.SAFE;
+                return this.nextState;
+            }
+
+            if ()
+
+
+                // us.location.distance(ourGoal) )
+//            Robot us = Strategy.world.getRobot(this.robot.robotType);
+//            if (us == null) {
+//                // TODO: Angry yelling
+//            } else {
+//                VectorGeometry ourGoal = new VectorGeometry(-Constants.PITCH_WIDTH / 2, 0);
+//                if (us.location.distance(ourGoal) > ball.location.distance(ourGoal)) {
+//                    this.nextState = BehaviourEnum.SAFE;
+//                } else {
+////                    if(Math.abs(ball.location.x) > Constants.PITCH_WIDTH/2 - 20 && Math.abs(ball.location.y) > Constants.PITCH_HEIGHT/2 - 20){
+////                        this.nextState = BehaviourEnum.SHUNT;
+////                    } else {
+//                    boolean canKick = true;
+//                    boolean closer = true;
+//                    for (Robot r : Strategy.world.getRobots()) {
+//                        if (r != null && r.type != RobotType.FRIEND_2 && r.velocity.length() < 1)
+//                            canKick = canKick && r.location.distance(ball.location) > 5;
+//                            closer = closer && us.location.distance(ball.location)< r.location.distance(ball.location);
+//                    }
+//                    if (canKick && (this.lastState != BehaviourEnum.DEFEND ||
+//                            VectorGeometry.angle(ball.velocity, VectorGeometry.fromTo(ball.location, ourGoal)) > 2)
+//                            || closer) {
+//                        this.nextState = BehaviourEnum.GOAL;
+//                    } else {
+//                        this.nextState = BehaviourEnum.DEFEND;
+//                    }
+////                    }
+//                }
+//            }
         }
         return this.nextState;
     }
